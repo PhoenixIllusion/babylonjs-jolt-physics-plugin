f7f32e06eb8a492668f16091877e567910c44523
Jonathan Baran <jonathan.baran@vmlyr.com>
2024-02-07 15:12:35 -0600

    Build: Demo - fix basic-example RollUp config

--- basic-examples/vite.config.ts
+++ basic-examples/vite.config.ts
@@ -5,12 +5,14 @@ export default defineConfig({
     rollupOptions: {
       input: {
         "2d_funnel": "2d_funnel.html",
+        "add_remove_bodies": "add_remove_bodies.html",
+        "bounce_sphere": "bounce_sphere.html",
         "character_virtual": "character_virtual.html",
         "conveyor_belt": "conveyor_belt.html",
         "falling_shapes": "falling_shapes.html",
         "friction": "friction.html",
-        "bounce_sphere": "bounce_sphere.html",
         "motor": "motor.html",
+        "ray_cast": "ray_cast.html",
         "vehicle_wheeled": "vehicle_wheeled.html",
         "vehicle_motorcycle": "vehicle_motorcycle.html"
       },
